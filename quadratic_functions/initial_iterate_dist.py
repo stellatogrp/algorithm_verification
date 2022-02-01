5e53b9dbcc09eff29ab342ef9dd1d0928d5e198d
Vinit Ranjan <vinitranjan821@gmail.com>
2022-02-01 01:16:42 -0500

    pushed GD to separate file

--- quadratic_functions/initial_iterate_dist.py
+++ quadratic_functions/initial_iterate_dist.py
@@ -2,20 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from generate_quadratic_function import *
-
-
-def grad_descent_step(grad, x_0, gamma=0.01):
-    grad_step = grad(x_0)
-    return x_0 - gamma * grad_step
-
-
-def grad_descent(grad, x_0, gamma, num_iter, tol=1e-8):
-    iterates = []
-    for i in range(num_iter):
-        iterates.append(x_0)
-        x_0 = grad_descent_step(grad, x_0, gamma)
-    iterates.append(x_0)
-    return iterates
+from gradient_descent import *
 
 
 def generate_uniform_circle(num_points, R):
