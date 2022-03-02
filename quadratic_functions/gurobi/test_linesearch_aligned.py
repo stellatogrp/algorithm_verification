a402f8bbd705e3bef5fc691d22a6d87e3591a5b6
Vinit Ranjan <vinitranjan821@gmail.com>
2022-03-02 15:40:28 -0500

    added checks for rewritten gurobi call and artifical upper bound

--- quadratic_functions/gurobi/test_linesearch_aligned.py
+++ quadratic_functions/gurobi/test_linesearch_aligned.py
@@ -37,7 +37,7 @@ def test_linesearch_aligned():
     Z = spa.csc_matrix((n, n))
     P = spa.csc_matrix([[mu, 0], [0, L]])
 
-    Hobj = .5 * spa.bmat([[Z, Z], [Z, P]], format='csc')  # not negative since we maximize
+    Hobj = -.5 * spa.bmat([[Z, Z], [Z, P]], format='csc')
     #  cobj = np.zeros(2 * n)
     #  dobj = 0
 
@@ -57,33 +57,19 @@ def test_linesearch_aligned():
                   name='y',
                   ub=gp.GRB.INFINITY * np.ones(2 * n),
                   lb=-gp.GRB.INFINITY * np.ones(2 * n))
-    m.setObjective(y @ Hobj @ y, GRB.MAXIMIZE)
+    m.setObjective(y @ Hobj @ y, GRB.MINIMIZE)
     m.addConstr(y @ Hineq @ y <= R ** 2)
     m.addConstr(y @ Heq1 @ y == 0)
     m.addConstr(y @ Heq2 @ y == 0)
+    m.addConstr(y @ Hobj @ y <= -.75)  # fake constraint to give gurobi an upper bound
 
-    # Trying with more declarative variables
-    #  Pblock = spa.bmat([[P, Z], [Z, P]], format='csc')
-    #  m = gp.Model()
-    #  m.setParam('NonConvex', 2)
-    #
-    #  x = m.addMVar(2 * n,
-    #                name='x',
-    #                ub=gp.GRB.INFINITY * np.ones(2 * n),
-    #                lb=-gp.GRB.INFINITY * np.ones(2 * n))
-    #  y = m.addMVar(2 * n,
-    #                name='y',
-    #                ub=gp.GRB.INFINITY * np.ones(2 * n),
-    #                lb=-gp.GRB.INFINITY * np.ones(2 * n))
-    #
-    #  m.setObjective(x @ Hobj @ x, GRB.MAXIMIZE)
-    #  m.addConstr(y == Pblock @ x)
-    #  m.addConstr(y[n:] @ x[n:] - y[n:] @ x[:n] <= 1e-03)
-    #  m.addConstr(y[n:] @ y[:n] <= 1e-03)
-    #  m.addConstr(x[:n] @ x[:n] <= R ** 2)
-    #
     m.optimize()
-    m.printAttr('y')
+    # import pdb
+    # pdb.set_trace()
+    y_grb = m.getVars()
+    y = np.array([y_grb[i].X for i in range(2 * n)])
+    print(y)
+    # m.printAttr('y')
 
 
 def main():
