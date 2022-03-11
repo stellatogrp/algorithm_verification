f9fde94b3f7cb845c72ad39d7500537ea3aa449b
Vinit Ranjan <vinitranjan821@gmail.com>
2022-03-11 15:41:05 -0500

    added ISTA Gurobi test

--- test_ISTA_quad_extractor.py
+++ test_ISTA_quad_extractor.py
@@ -1,12 +1,19 @@
 import numpy as np
 import cvxpy as cp
+import gurobipy as gp
 import scipy.sparse as spa
 from qcqp2quad_form.quad_extractor import QuadExtractor
 from cvxpy.constraints import Equality, Inequality, NonNeg, NonPos, Zero
 from quadratic_functions.extended_slemma_sdp import solve_full_extended_slemma_primal_sdp, solve_full_extended_slemma_dual_sdp
+from gurobipy import GRB
 
 
-def test_ISTA():
+def eval_lasso(A, b, x, lambd):
+    c = A @ x - b
+    return .5 * c.T @ c + lambd * np.linalg.norm(x, 1)
+
+
+def form_problem_and_extract():
     np.random.seed(0)
 
     n = 2
@@ -60,6 +67,11 @@ def test_ISTA():
 
     problem = cp.Problem(cp.Maximize(obj), constraints)
     quad_extractor = QuadExtractor(problem)
+    return quad_extractor
+
+
+def test_ISTA_SDR():
+    quad_extractor = form_problem_and_extract()
     data_objective = quad_extractor.extract_objective()
     data_constraints, data_constr_types = quad_extractor.extract_constraints()
     # print(data_objective)
@@ -102,8 +114,56 @@ def test_ISTA():
     # print(np.round(N, 4))
 
 
+def test_ISTA_Gurobi():
+    quad_extractor = form_problem_and_extract()
+    data_objective = quad_extractor.extract_objective()
+    data_constraints, data_constr_types = quad_extractor.extract_constraints()
+
+    n = data_objective['q'].shape[0]
+    Hobj = data_objective['P']
+    cobj = data_objective['q']
+    dobj = data_objective['r']
+
+    print('-------- solving with gurobi --------')
+    print(n)
+    m = gp.Model()
+    m.setParam('NonConvex', 2)
+
+    x = m.addMVar(n,
+                  name='x',
+                  ub=gp.GRB.INFINITY * np.ones(n),
+                  lb=-gp.GRB.INFINITY * np.ones(n))
+
+    obj = x @ Hobj @ x + cobj @ x + dobj
+    m.setObjective(obj, GRB.MAXIMIZE)
+
+    for i in range(len(data_constraints)):
+        constr = data_constraints[i]
+        Hcons = constr['P']
+        # if Hcons is None:
+        #     Hcons = spa.csc_matrix(np.zeros((n, n)))
+        ccons = constr['q']
+        dcons = constr['r']
+        constr_type = data_constr_types[i]
+        if constr_type in (Inequality, NonPos, NonNeg):
+            if Hcons is None:
+                m.addConstr(ccons @ x + dcons <= 0)
+            else:
+                m.addConstr(x @ Hcons @ x + ccons @ x + dcons <= 0)
+        else:
+            # eq_triples.append((Hcons, ccons, dcons))
+            if Hcons is None:
+                m.addConstr(ccons @ x + dcons == 0)
+            else:
+                m.addConstr(x @ Hcons @ x + ccons @ x + dcons == 0)
+
+    m.optimize()
+    print(x.X)
+
+
 def main():
-    test_ISTA()
+    # test_ISTA_SDR()
+    test_ISTA_Gurobi()
 
 
 if __name__ == '__main__':
