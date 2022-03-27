05e74f3d1961edcd698b97eb6f107a679338da0a
Vinit Ranjan <vinitranjan821@gmail.com>
2022-03-27 19:46:43 -0400

    basic GD with PEPit

--- PEPit_comp/test_basic_pepit.py
+++ PEPit_comp/test_basic_pepit.py
@@ -4,7 +4,49 @@ from PEPit.functions import SmoothStronglyConvexFunction
 
 
 def main():
-    pass
+    problem = PEP()
+    L = 3
+    mu = .1
+    gamma = 1 / L
+    n = 4
+    verbose = 1
+
+    # Declare a strongly convex smooth function
+    func = problem.declare_function(SmoothStronglyConvexFunction, param={'L': L, 'mu': mu})
+
+    # Start by defining its unique optimal point xs = x_* and corresponding function value fs = f_*
+    xs = func.stationary_point()
+    fs = func.value(xs)
+
+    # Then define the starting point x0 of the algorithm
+    x0 = problem.set_initial_point()
+
+    # Set the initial constraint that is the distance between x0 and x^*
+    problem.set_initial_condition((x0 - xs) ** 2 <= 1)
+
+    # Run n steps of the GD method
+    x = x0
+    for _ in range(n):
+        x = x - gamma * func.gradient(x)
+
+    # Set the performance metric to the function values accuracy
+    problem.set_performance_metric(func.value(x) - fs)
+
+    # Solve the PEP
+    pepit_verbose = max(verbose, 0)
+    pepit_tau = problem.solve(verbose=pepit_verbose)
+
+    # Compute theoretical guarantee (for comparison)
+    theoretical_tau = L / (2 * (2 * n * L * gamma + 1))
+
+    # Print conclusion if required
+    if verbose != -1:
+        print('*** Example file: worst-case performance of gradient descent with fixed step-sizes ***')
+        print('\tPEPit guarantee:\t f(x_n)-f_* <= {:.6} ||x_0 - x_*||^2'.format(pepit_tau))
+        print('\tTheoretical guarantee:\t f(x_n)-f_* <= {:.6} ||x_0 - x_*||^2'.format(theoretical_tau))
+
+    # Return the worst-case guarantee of the evaluated method (and the reference theoretical value)
+    return pepit_tau, theoretical_tau
 
 
 if __name__ == '__main__':
