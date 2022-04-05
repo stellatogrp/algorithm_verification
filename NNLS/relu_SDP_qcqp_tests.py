f517c028a825b5ec751d8d1bb2862ed9edc57da9
Vinit Ranjan <vinitranjan821@gmail.com>
2022-04-04 23:57:49 -0400

    working PEP with RELU after adding quadratic constraints

--- NNLS/relu_SDP_qcqp_tests.py
+++ NNLS/relu_SDP_qcqp_tests.py
@@ -145,33 +145,119 @@ def test_relu_Gurobi():
     print('x:', np.round(x.X, 4))
 
 
+def test_cvxpy_matrix_products():
+    n = 2
+    P = cp.Variable((2 * n + 1, 2 * n + 1), symmetric=True)
+    Q = cp.Variable((2 * n + 1, 2 * n + 1), symmetric=True)
+    np.random.seed(1)
+
+    A = np.random.randn(2 * n + 1, 2 * n + 1)
+    B = np.random.randn(2 * n + 1, 2 * n + 1)
+
+    obj = cp.sum(Q)
+    constraints = [P >> 0, Q == A @ P @ B]
+    constraints.append(P <= 1)
+
+    problem = cp.Problem(cp.Maximize(obj), constraints)
+    result = problem.solve()
+    print(result)
+    print(P.value)
+    print(np.round(Q.value, 4))
+
+
 def test_specific_relu_SDP_ndim():
+    m = 3
     n = 2
     print('--------testing n=%d with general--------' % n)
     P = cp.Variable((2 * n + 1, 2 * n + 1), symmetric=True)
-    np.random.seed(1)
-    c = np.random.randn(n)
+    np.random.seed(0)
+    A = np.random.randn(m, n)
+    b = np.random.randn(m)
+    halfATA = .5 * A.T @ A
+    bTA = b.T @ A
+
     l = -2
     u = -1
+    R = 1
 
-    obj = c.T @ P[0:n, -1]
+    # obj = c.T @ P[0:n, -1]
+    obj = cp.trace(halfATA @ P[0: n, 0: n]) - bTA @ P[0:n, -1] + .5 * b.T @ b
     # obj = cp.sum_squares(P[0:n, -1])
     constraints = []
     constraints.append(P[0:n, -1] >= 0)
     constraints.append(P[0:n, -1] >= P[n:2*n, -1])
-
     constraints.append(cp.diag(P[0: n, 0: n]) == cp.diag(P[0: n, n: 2 * n]))
-    constraints.append(cp.diag(P[n: 2 * n, n: 2 * n]) <= (l + u) * P[n: 2 * n, -1] - l * u)
-    # constraints.append(cp.sum_squares(cp.diag(P[n: 2 * n, n: 2 * n])) <= 1)
+
+    # constraints.append(cp.diag(P[n: 2 * n, n: 2 * n]) <= (l + u) * P[n: 2 * n, -1] - l * u)
+    constraints.append(cp.trace(P[n: 2 * n, n: 2 * n]) <= R ** 2)
+    # constraints.append(cp.sum_squares(P[n: 2 * n, -1]) <= R ** 2)
 
     constraints.append(P[-1, -1] == 1)
     constraints.append(P >> 0)
 
-    problem = cp.Problem(cp.Minimize(obj), constraints)
+    problem = cp.Problem(cp.Maximize(obj), constraints)
     result = problem.solve(solver=cp.MOSEK, verbose=True)
-    print('c:', c)
     print(result)
     print(np.round(P.value, 4))
+
+
+def test_PEP_RELU():
+    np.random.seed(0)
+
+    n = 2
+    m = 3
+    t = .05
+    R = 1
+
+    A = np.random.randn(m, n)
+    b = np.random.randn(m)
+    I = np.eye(n)
+    halfATA = .5 * A.T @ A
+    bTA = b.T @ A
+    tbTA = t * b.T @ A
+    tATb = t * A.T @ b
+    C = I - t * A.T @ A
+
+    print(A, b)
+
+    print('--------testing n=%d with general--------' % n)
+    P = cp.Variable((3 * n + 1, 3 * n + 1), symmetric=True)
+    l = 0
+    u = 1
+
+    # P = v v.T , where v = [z y x 1] stack
+    # x is initial point, y is gradient step from x, z = RELU(x)
+    obj = cp.trace(halfATA @ P[0: n, 0: n]) - bTA @ P[0:n, -1] + .5 * b.T @ b
+    constraints = [P >> 0, P[-1, -1] == 1]
+
+    # initial bounds
+    constraints.append(cp.diag(P[2 * n: 3 * n, 2 * n: 3 * n]) <= (l + u) * P[2 * n: 3 * n, -1] - l * u)
+    # constraints.append(cp.trace(P[2 * n: 3 * n, 2 * n: 3 * n]) <= R ** 2)
+
+    # relu constraints
+    constraints.append(P[0:n, -1] >= 0)
+    constraints.append(P[0: n, -1] >= P[n: 2 * n, -1])
+    constraints.append(cp.diag(P[0: n, 0: n]) == cp.diag(P[0: n, n: 2 * n]))
+
+    # gradient step
+    constraints.append(P[n: 2 * n, -1] == C @ P[2 * n: 3 * n, -1] + tATb)
+    constraints.append(P[n: 2 * n, n: 2 * n]
+                       == C @ P[2 * n: 3 * n, 2 * n: 3 * n] @ C.T
+                       + C @ cp.reshape(P[2 * n: 3 * n, -1], (n, 1)) @ tATb.reshape(1, n)
+                       + tATb.reshape(n, 1) @ cp.reshape(P[2 * n: 3 * n, -1], (1, n)) @ C.T
+                       + np.outer(tATb, tATb))
+
+    # print(C @ cp.reshape(P[2 * n: 3 * n, -1], (n, 1)) @ tATb.reshape(1, n))
+    # print((C @ cp.reshape(P[2 * n: 3 * n, -1], (n, 1)) @ tATb.reshape(1, n)).shape)
+    # print((tATb.reshape(n, 1) @ cp.reshape(P[2 * n: 3 * n, -1], (1, n)) @ C.T).shape)
+    # print(np.outer(tATb, tATb).shape)
+
+    problem = cp.Problem(cp.Maximize(obj), constraints)
+    result = problem.solve(solver=cp.MOSEK, verbose=True)
+    print(result)
+    print(np.round(P.value, 4))
+
+    print(C @ np.array([1, 1]) + tATb)
 
 
 def main():
@@ -179,6 +265,8 @@ def main():
     # test_NNLS_Gurobi()
     # test_NNLS_SDR()
     test_specific_relu_SDP_ndim()
+    # test_cvxpy_matrix_products()
+    test_PEP_RELU()
 
 
 if __name__ == '__main__':
