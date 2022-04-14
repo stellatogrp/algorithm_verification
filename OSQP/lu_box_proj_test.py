a92737046a6ec9a1c60e21080764204f961c8901
Vinit Ranjan <vinitranjan821@gmail.com>
2022-04-13 22:54:07 -0400

    added shell for lu box proj test

---
+++ OSQP/lu_box_proj_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cvxpy as cp
+import scipy.sparse as spa
+import gurobipy as gp
+from qcqp2quad_form.quad_extractor import QuadExtractor
+from cvxpy.constraints import Equality, Inequality, NonNeg, NonPos, Zero
+from quadratic_functions.extended_slemma_sdp import *
+
+from gurobipy import GRB
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
