5fa36ef0a13299ef4335eb55e4d091ed8e74718a
Vinit Ranjan <vinitranjan821@gmail.com>
2022-04-17 16:05:38 -0400

    added one step OSQM ADMM PEP with all moving parts

--- OSQP/OSQP_ADMM_PEP.py
+++ OSQP/OSQP_ADMM_PEP.py
@@ -13,13 +13,13 @@ def test_with_cvxpy(n, P, q, A, l, u):
     prob = cp.Problem(obj, constraints)
     res = prob.solve()
     print(res)
-    print('x =\n', np.round(x.value, 4))
+    # print('x =\n', np.round(x.value, 4))
 
 
 def test_osqp_admm_onestep_pep():
-    m = 4
-    n = 5
-    R = 1
+    m = 10
+    n = 20
+    R = 10
     In = np.eye(n)
     Im = np.eye(m)
 
@@ -30,6 +30,7 @@ def test_osqp_admm_onestep_pep():
     # print(P, np.linalg.eigvals(P))
     q = np.random.randn(n)
     q = q.reshape(n, 1)
+    # print(q)
 
     # constraints for minimization obj
     A = np.random.randn(m, n)
@@ -158,18 +159,19 @@ def test_osqp_admm_onestep_pep():
 
 def test_osqp_admm_onestep_pep_mult_moving_parts():
     print('--------solving pep with more moving parts--------')
-    m = 4
-    n = 5
-    R = 1
+    m = 10
+    n = 20
+    R = 10
     In = np.eye(n)
+    Zmn = np.zeros((m, n))
     Im = np.eye(m)
 
     np.random.seed(0)
     Phalf = np.random.randn(n, n)
     P = Phalf @ Phalf.T
-    # print(P, np.linalg.eigvals(P))
-    # q = np.random.randn(n)
-    # q = q.reshape(n, 1)
+    q_val = np.random.randn(n)
+    q_val = q_val.reshape(n, 1)
+    # print(q_val)
 
     A = np.random.randn(m, n)
     l = -1 * np.ones(m)
@@ -182,6 +184,125 @@ def test_osqp_admm_onestep_pep_mult_moving_parts():
     rho_inv = 1 / rho
 
     C = np.block([P + sigma * In + rho * A.T @ A, -rho * A.T, A.T, -sigma * In, In])
+    H = np.block([rho * A, -rho * Im, Im, Zmn, Zmn])
+    J = np.block([A, rho_inv * Im])
+
+    # bounds on q
+    q = cp.Variable((n, 1))
+    ql = 1
+    q2 = 2
+
+    # initial iterates
+    x0 = cp.Variable((n, 1))
+    y0 = cp.Variable((m, 1))
+    z0 = cp.Variable((m, 1))
+
+    # next x iterate
+    x1 = cp.Variable((n, 1))
+
+    # outer products for x1 z0 y0 x0 q
+    x1x1T = cp.Variable((n, n))
+    x1z0T = cp.Variable((n, m))
+    x1y0T = cp.Variable((n, m))
+    x1x0T = cp.Variable((n, n))
+    x1qT = cp.Variable((n, n))
+
+    z0z0T = cp.Variable((m, m))
+    z0y0T = cp.Variable((m, m))
+    z0x0T = cp.Variable((m, n))
+    z0qT = cp.Variable((m, n))
+
+    y0y0T = cp.Variable((m, m))
+    y0x0T = cp.Variable((m, n))
+    y0qT = cp.Variable((m, n))
+
+    x0x0T = cp.Variable((n, n))
+    x0qT = cp.Variable((n, n))
+
+    qqT = cp.Variable((n, n))
+
+    # form v0 and v0v0T for x1 update
+    v0 = cp.vstack([x1, z0, y0, x0, q])
+    v0v0T = cp.bmat([
+        [x1x1T, x1z0T, x1y0T, x1x0T, x1qT],
+        [x1z0T.T, z0z0T, z0y0T, z0x0T, z0qT],
+        [x1y0T.T, z0y0T.T, y0y0T, y0x0T, y0qT],
+        [x1x0T.T, z0x0T.T, y0x0T.T, x0x0T, x0qT],
+        [x1qT.T, z0qT.T, y0qT.T, x0qT.T, qqT]
+    ])
+
+    # initial constraints
+    constraints = [cp.sum_squares(x0) <= R ** 2, cp.trace(x0x0T) <= R ** 2,
+                   # cp.reshape(cp.diag(x0x0T), (n, 1)) <= 1,
+                   y0 == 0, y0y0T == 0,
+                   # cp.sum_squares(y0) <= .1 * R ** 2, cp.trace(y0y0T) <= .1 * R ** 2,
+                   z0 == 0, z0z0T == 0,
+                   q == q_val, qqT == np.outer(q_val, q_val),
+                   # cp.sum_squares(q) <= .1 * R ** 2, cp.trace(qqT) <= .1 * R ** 2
+                   ]
+
+    # constraints to create x1
+    constraints += [C @ v0 == 0, C @ v0v0T @ C.T == 0,
+                    cp.bmat([
+                        [v0v0T, v0],
+                        [v0.T, np.array([[1]])]
+                    ]) >> 0]
+
+    y1 = H @ v0
+    y1y1T = H @ v0v0T @ H.T
+
+    # constraints for z^{k+1} i.e. box projection
+    wtilde0 = cp.vstack([x1, y1])
+    # x1y1T = cp.bmat([[x1x1T, x1y0T, x1z0T]]) @ H.T
+    x1y1T = cp.Variable((n, m))  # TODO: check this
+    wtilde0wtilde0T = cp.bmat([[x1x1T, x1y1T],
+                               [x1y1T.T, y1y1T]
+                               ])
+    constraints += [cp.bmat([[wtilde0wtilde0T, wtilde0],
+                             [wtilde0.T, np.array([[1]])]]) >> 0]
+
+    w0 = J @ wtilde0
+    w0w0T = J @ wtilde0wtilde0T @ J.T
+
+    z1 = cp.Variable((m, 1))
+    alpha1 = cp.Variable((m, 1))
+    z1z1T = cp.Variable((m, m))
+    z1alpha1T = cp.Variable((m, m))
+    z1w0T = cp.Variable((m, m))
+    alpha1alphaT = cp.Variable((m, m))
+    alpha1w0T = cp.Variable((m, m))
+    P1 = cp.bmat([[z1z1T, z1alpha1T, z1w0T, z1],
+                  [z1alpha1T.T, alpha1alphaT, alpha1w0T, alpha1],
+                  [z1w0T.T, alpha1w0T.T, w0w0T, w0],
+                  [z1.T, alpha1.T, w0.T, np.array([[1]])]
+                  ])
+
+    constraints += [alpha1 >= w0, alpha1 >= l,
+                    cp.diag(alpha1alphaT - alpha1w0T - l @ alpha1.T + l @ w0.T) == 0,
+                    z1 <= alpha1, z1 <= u,
+                    cp.diag(u @ alpha1.T - u @ z1.T - z1alpha1T + z1z1T) == 0,
+                    # P1 >> 0, u0u0T >> 0, v0v0T >> 0, wtilde0wtilde0T >> 0
+                    P1 >> 0,
+                    ]
+
+    z1z0T = cp.Variable((m, m))
+    y1y0T = cp.Variable((m, m))
+    Z = cp.bmat([[z1z1T, z1z0T, z1],
+                 [z1z0T.T, z0z0T, z0],
+                 [z1.T, z0.T, np.array([[1]])]
+                 ])
+    Y = cp.bmat([[y1y1T, y1y0T, y1],
+                 [y1y0T.T, y0y0T, y0],
+                 [y1.T, y0.T, np.array([[1]])]
+                 ])
+    constraints += [Y >> 0, Z >> 0]
+
+    obj = cp.Maximize(cp.trace(x1x1T - 2 * x1x0T + x0x0T)
+                      + cp.trace(y1y1T - 2 * y1y0T + y0y0T)
+                      + cp.trace(z1z1T - 2 * z1z0T + z0z0T))
+    prob = cp.Problem(obj, constraints)
+    res = prob.solve(solver=cp.MOSEK, verbose=True)
+    print('sdp result =', res)
 
 
 def main():
