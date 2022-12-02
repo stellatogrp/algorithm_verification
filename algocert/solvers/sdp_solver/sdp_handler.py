b16d59bec26968110fee7659debbdb64c81c6577
Vinit Ranjan <vinitranjan821@gmail.com>
2022-12-02 01:29:46 -0500

    fixed rlt bug and started experiment with small param eps

--- algocert/solvers/sdp_solver/sdp_handler.py
+++ algocert/solvers/sdp_solver/sdp_handler.py
@@ -120,6 +120,16 @@ class SDPHandler(object):
         self.initialize_init_set_bounds()
         self.initialize_param_set_bounds()
         self.propagate_iterate_bounds()
+        # for i in self.param_list:
+        #     print('param', i)
+        #     print(self.sdp_param_vars[i].get_lower_bound())
+        # for i, handler in enumerate(self.iteration_handlers):
+        #     print('handler', i)
+        #     for iterate in self.iterate_list:
+        #         # print(handler.iterate_vars[iterate])
+        #         print(iterate)
+        #         print(handler.iterate_vars[iterate].get_lower_bound())
+        # exit(0)
 
     def initialize_init_set_bounds(self):
         for init_set in self.CP.get_init_sets():
@@ -155,8 +165,8 @@ class SDPHandler(object):
 
     def canonicalize(self):
         self.convert_hl_to_basic_steps()
-        print('----BASIC STEP SDP----')
-        self.CP.print_cp()
+        # print('----BASIC STEP SDP----')
+        # self.CP.print_cp()
 
         self.create_iterate_id_maps()
         self.compute_sdp_param_vars()
--- experiments/OSQP/test_OSQP_gurobi.py
+++ experiments/OSQP/test_OSQP_gurobi.py
@@ -36,7 +36,7 @@ def OSQP_cert_prob(n, m, N=1, t=.05, xset=None, bset_func=None):
     u = 4 * np.ones((m, 1))
     sigma = 1
     rho = 1
-    # rho_inv = 1 / rho
+    rho_inv = 1 / rho
 
     x = Iterate(n, name='x')
     y = Iterate(m, name='y')
@@ -69,13 +69,13 @@ def OSQP_cert_prob(n, m, N=1, t=.05, xset=None, bset_func=None):
     step5 = MinWithVecStep(z, z_tilde, u=u)
 
     # step 6 for fixed point residual
-    # s = Iterate(m, name='s')
-    # s6_D = Im
-    # s6_A = spa.bmat([[Im, rho_inv * Im]])
-    # step6 = HighLevelLinearStep(s, [z, y], D=s6_D, A=s6_A, b=zeros_m, Dinv=s6_D)
+    s = Iterate(m, name='s')
+    s6_D = Im
+    s6_A = spa.bmat([[Im, rho_inv * Im]])
+    step6 = HighLevelLinearStep(s, [z, y], D=s6_D, A=s6_A, b=zeros_m, Dinv=s6_D)
 
-    steps = [step1, step2, step3, step4, step5]
-    # steps = [step1, step2, step3, step4, step5, step6]
+    # steps = [step1, step2, step3, step4, step5]
+    steps = [step1, step2, step3, step4, step5, step6]
 
     # xset = CenteredL2BallSet(x, r=r)
     x_l = -1 * np.ones((n, 1))
@@ -102,8 +102,8 @@ def OSQP_cert_prob(n, m, N=1, t=.05, xset=None, bset_func=None):
     test_set = BoxSet(test, test_l, test_u)
     bset = BoxStackSet(b, [test_set, [offset_zeros, offset_zeros]])
 
-    obj = [ConvergenceResidual(x), ConvergenceResidual(y), ConvergenceResidual(z)]
-    # obj = [ConvergenceResidual(x), ConvergenceResidual(s)]
+    # obj = [ConvergenceResidual(x), ConvergenceResidual(y), ConvergenceResidual(z)]
+    obj = [ConvergenceResidual(x), ConvergenceResidual(s)]
     # obj = OuterProdTrace(x)
 
     CP = CertificationProblem(N, [xset, yset, zset], [test_set, bset], obj, steps)
@@ -118,11 +118,105 @@ def OSQP_cert_prob(n, m, N=1, t=.05, xset=None, bset_func=None):
     print('global', resg)
 
 
+def OSQP_CP_noboxstack(n, m, N=1, t=.05, xset=None, bset_func=None):
+    #  r = 1
+
+    In = spa.eye(n)
+    Im = spa.eye(m)
+
+    np.random.seed(0)
+    A = np.random.randn(m, n)
+    A = spa.csc_matrix(A)
+    ATA = A.T @ A
+    Phalf = np.random.randn(n, n)
+    P = Phalf.T @ Phalf
+    # print(A)
+
+    # b_const = spa.csc_matrix(np.zeros((n, 1)))
+    zeros_n = np.zeros((n, 1))
+    zeros_m = np.zeros((m, 1))
+    l = 2 * np.ones((m, 1))
+    u = 4 * np.ones((m, 1))
+    sigma = 1
+    rho = 1
+    rho_inv = 1 / rho
+
+    x = Iterate(n, name='x')
+    y = Iterate(m, name='y')
+    w = Iterate(m, name='w')
+    z_tilde = Iterate(m, name='z_tilde')
+    z = Iterate(m, name='z')
+    b = Parameter(n, name='b')
+
+    # step 1
+    s1_Dtemp = P + sigma * In + rho * ATA
+    s1_Atemp = spa.bmat([[sigma * In, rho*A.T, -rho * A.T, -In]])
+    s1_D = In
+    s1_A = spa.csc_matrix(np.linalg.inv(s1_Dtemp) @ s1_Atemp)
+    step1 = HighLevelLinearStep(x, [x, z, y, b], D=s1_D, A=s1_A, b=zeros_n, Dinv=s1_D)
+
+    # step 2
+    s2_D = Im
+    s2_A = spa.bmat([[Im, rho * A, rho * Im]])
+    step2 = HighLevelLinearStep(y, [y, x, z], D=s2_D, A=s2_A, b=zeros_m, Dinv=s2_D)
+
+    # step 3
+    s3_D = Im
+    s3_A = spa.bmat([[A, 1/rho * Im]])
+    step3 = HighLevelLinearStep(w, [x, y], D=s3_D, A=s3_A, b=zeros_m, Dinv=s3_D)
+
+    # step 4
+    step4 = MaxWithVecStep(z_tilde, w, l=l)
+
+    # step 5
+    step5 = MinWithVecStep(z, z_tilde, u=u)
+
+    # step 6 for fixed point residual
+    s = Iterate(m, name='s')
+    s6_D = Im
+    s6_A = spa.bmat([[Im, rho_inv * Im]])
+    step6 = HighLevelLinearStep(s, [z, y], D=s6_D, A=s6_A, b=zeros_m, Dinv=s6_D)
+
+    # steps = [step1, step2, step3, step4, step5]
+    steps = [step1, step2, step3, step4, step5, step6]
+
+    # xset = CenteredL2BallSet(x, r=r)
+    x_l = -.1 * np.ones((n, 1))
+    x_u = 0.1 * np.ones((n, 1))
+    xset = BoxSet(x, x_l, x_u)
+    # xset = ConstSet(x, np.zeros((n, 1)))
+
+    yset = ConstSet(y, np.zeros((m, 1)))
+
+    zset = ConstSet(z, np.zeros((m, 1)))
+
+    # b_l = np.zeros((n, 1))
+    # b_u = .05 * np.ones((n, 1))
+    # bset = BoxSet(b, b_l, b_u)
+    bset = ConstSet(b, 0.5 * np.ones((n, 1)))
+
+    # obj = [ConvergenceResidual(x), ConvergenceResidual(y), ConvergenceResidual(z)]
+    obj = [ConvergenceResidual(x), ConvergenceResidual(s)]
+    # obj = OuterProdTrace(x)
+
+    CP = CertificationProblem(N, [xset, yset, zset], [bset], obj, steps)
+
+    CP.print_cp()
+
+    # res = CP.solve(solver_type='SDP', add_RLT=False, verbose=True)
+    # print('sdp', res)
+    res = CP.solve(solver_type='SDP', add_RLT=True, verbose=True)
+    print('sdp rlt', res)
+    # resg = CP.solve(solver_type='GLOBAL', add_bounds=True, TimeLimit=3600)
+    # print('global', resg)
+
+
 def main():
-    m = 10
-    n = 6
+    m = 5
+    n = 3
     N = 1
-    OSQP_cert_prob(n, m, N=N)
+    # OSQP_cert_prob(n, m, N=N)
+    OSQP_CP_noboxstack(n, m, N=N)
 
 
 if __name__ == '__main__':
