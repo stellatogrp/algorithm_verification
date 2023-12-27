6b99d6f2ac52667c7d23df1e477b560ecb33d965
Vinit Ranjan <vinitranjan821@gmail.com>
2023-12-26 20:01:43 -0500

    updating silver experiment

--- paper_experiments/silver/silver_strongcvx_NNLS.py
+++ paper_experiments/silver/silver_strongcvx_NNLS.py
@@ -142,60 +142,44 @@ def silver_vs_opt_sdp(m, n, b_cmul, b_r, instance, K_max, seed):
         # out_df.to_csv('data/strongcvx/sdp_silver_m15n8_rhalf.csv', index=False)
 
 
-def t_fixed_sdp(m, n, b_cmul, b_r, instance, K_max, seed):
-    L = instance.L
-    1 / L
-    t_opt = instance.get_t_opt()
-    t_max = 2 / L
-    # t_vals = [t_min, (t_opt + t_min) / 2, (t_opt + t_max) / 2]
-    # t_vals = [t_min, np.sqrt(t_opt * t_min), np.sqrt(t_opt * t_max)]
-    # t_vals = [t_min, np.sqrt(t_opt * t_min), t_max]
-    tval_eps = t_max - t_opt
-    t_vals = [t_opt - 2 * tval_eps, t_opt - 1 * tval_eps, t_opt + 0.75 * tval_eps]
-    print(t_opt, t_vals)
+def double_silver_experiments():
+    d = datetime.now()
+    # print(d)
+    curr_time = d.strftime('%m%d%y_%H%M%S')
+    outf_prefix = '/home/vranjan/algorithm-certification/'
+    # outf_prefix = '/Users/vranjan/Dropbox (Princeton)/ORFE/2022/algorithm-certification/'
+    outf = outf_prefix + f'paper_experiments/NNLS/data/{curr_time}.csv'
+    print(outf)
 
-    K_vals = range(1, K_max + 1)
-    # K_vals = range(K_max, K_max + 1)
-    out_res = []
-    for K in K_vals:
-        for t in t_vals:
-            CP2 = instance.generate_CP(t, K)
-            out = CP2.solve(solver_type='SDP_CUSTOM')
-            out['orig_m'] = m
-            out['orig_n'] = n
-            out['mu'] = instance.mu
-            out['L'] = instance.L
-            out['b_cmul'] = b_cmul
-            out['b_r'] = b_r
-            out['K'] = K
-            out['sched'] = 't_fixed'
-            out['t'] = t
-            out['seed'] = seed
-            # sdp_c, sdp_canontime, sdp_solvetime = out['sdp_objval'], out['sdp_canontime'], out['sdp_solvetime']
-            out_res.append(pd.Series(out))
-            out_df = pd.DataFrame(out_res)
-            # out_df.to_csv('data/strongcvx/sdp_tfixed_m15n8_rhalf.csv', index=False)
-        print(out_df)
+    # m, n = 30, 15
+    m, n = 60, 40
+    b_cmul = 20
+    b_c = b_cmul * np.ones((m, 1))
+    b_r = .5
+    seed = 1
+
+    instance = NNLS(m, n, b_c, b_r, ATA_mu=20, seed=seed)
+    print(instance.A)
 
 
 def silver_vs_opt():
     # m, n = 30, 15
-    m, n = 60, 40
-    bc_mul = 20
-    b_c = bc_mul * np.ones((m, 1))
-    b_r = 1
-    K_max = 10
-    seed = 1
+    # m, n = 60, 40
+    # bc_mul = 20
+    # b_c = bc_mul * np.ones((m, 1))
+    # b_r = 1
+    # K_max = 10
+    # seed = 1
 
-    instance = NNLS(m, n, b_c, b_r, seed=seed)
-    print(instance.L, instance.mu, instance.kappa)
-    print(instance.get_t_opt())
+    # instance = NNLS(m, n, b_c, b_r, seed=seed)
+    # print(instance.L, instance.mu, instance.kappa)
+    # print(instance.get_t_opt())
     # compute_silver_idx(instance.kappa, 16)
     # compute_silver_steps(instance.kappa, 16)
     # silver_vs_opt_glob(m, n, b_c, b_r, instance, K_max)
 
-    # t_fixed_sdp(m, n, bc_mul, b_r, instance, K_max, seed)
-    silver_vs_opt_sdp(m, n, bc_mul, b_r, instance, K_max, seed)
+    # silver_vs_opt_sdp(m, n, bc_mul, b_r, instance, K_max, seed)
+    double_silver_experiments()
 
 
 def main():
